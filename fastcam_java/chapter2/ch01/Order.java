59b41938775d748e79ce201d184331ba58ef18e0
Junjehyun <h94051987@gmail.com>
2023-03-05 21:39:05 +0900

    230305 update2

--- fastcam_java/chapter2/ch01/Order.java
+++ fastcam_java/chapter2/ch01/Order.java
@@ -1,7 +1,8 @@
 package fastcam_java.chapter2.ch01;
 
 public class Order {
-    
+    // 여기서는 주문이라는 클래스에, 
+    // 주문번호, 구매자id, 판매자id, 제품id, 주문날짜 등이 멤버 변수이다.
     int orderId;
     String buyerId;
     String sellerId;
--- fastcam_java/chapter2/ch01/UserInfo.java
+++ fastcam_java/chapter2/ch01/UserInfo.java
@@ -1,7 +1,9 @@
 package fastcam_java.chapter2.ch01;
 
 public class UserInfo {
-    
+    // 여기서는 사용자정보라는 클래스에 
+    //유저id, 유저비밀번호, 유저이름, 주소, 폰번호 등이 멤버 변수이다.
+
     String userId;
     String userPassword;
     String userName;
