afe6e9e09f4e31bcee782c5a94efea0549894b72
Junjehyun <h94051987@gmail.com>
2023-03-15 15:16:24 +0900

    230315 Basic course complete

--- basic_course/Basic07.java
+++ basic_course/Basic07.java
@@ -45,16 +45,103 @@ public abstract class Animal {
             super(name,age);
             }
 
-        // Dog 클래스는 Animal클래스의 자식 클래스이다. 그리고 makeSound() 메서드를 구현했다.
-        // 이렇게 하위 클래스에서 추상 메서드를 구현 함으로써 실제 객체를 생성할 수 있다.    
-        public void makeSound() {
-            System.out.println("멍멍!");
+    // Dog 클래스는 Animal클래스의 자식 클래스이다. 그리고 makeSound() 메서드를 구현했다.
+    // 이렇게 하위 클래스에서 추상 메서드를 구현 함으로써 실제 객체를 생성할 수 있다.    
+    public void makeSound() {
+        System.out.println("멍멍!");
         }
-     }
-    
     }
+    
+}
 //------------------------------------------------------------------------------------------
 // 1-2 인터페이스 (Interface)
+// 인터페이스는 클래스와는 다르게, 멤버 변수를 가질 수 없고, 모든 메서드는 추상 메서드이다.
+// 그에 따라 모든 메서드는 하위 클래스에서 반드시 구현 되어야한다. 인터페이스는 특정한 기능에 표준화된
+// 규약을 정의하는 용도로 사용된다. 일종의 약속인건가 ? 
 
+// Drivable 인터페이스는 accelerate()와 brake()라는 추상 메서드를 선언하고 있다.
+// 이 인터페이스를 구현하는 클래스는 이 두(엑셀,브레이크) 메서드를 반드시 구현해야만 한다.
+public interface Drivable {
+    void accelerate(int speed);
+    void brake(int speed);
+
+    // implements는 인터페이스를 구현하는 클래스를 정의할때 사용됟ㅁ. 인터페이스는 추상적인 개념으로
+    // 클래스가 반드시 구현해야하는 메서드들의 시그니처를 정의한다. 
+    // implements 키워드는 클래스가 해당 인터페이스를 구현하고 있음을 나타내는 것.
+
+    public class Car implements Drivable {
+        private String name;
+
+        public Car(String name) {
+            this.name = name;
+        }
+        // 'Car' 클래스는 'Drivable' 인터페이스를 구현하고 있다. 따라서 
+        // accelerate()와 break() 메서드를 반드시 구현해야한다. 
+
+        // 요렇게 구현했다.
+        public void accelerate(int speed) {
+            System.out.println(name + " is accelerating at " + speed + " mph");
+        }
+
+        public void brake(int speed) {
+            System.out.println(name + " is braking at " + speed + " mph");
+        }
+    }
+}
+//------------------------------------------------------------------------------------------------
+// 2★인터페이스 상속
+// 인터페이스 상속(interface Inheritance)은 인터페이스가 다른 인터페이스를 상속하는걸 말한다.
+// 자바에서 인터페이스 상속은 extends 키워드를 사용하여 구현된다. 인터페이스 A가 인터페이스 B를 상속하면
+// A는 B의 메서드를 상속하고, A에서 새로운 메서드를 정의할 수 있다. 코드 예시로 보자
+
+    public interface B {
+        void methodB();
+    }
+    public interface A extends B {
+        void methodA();
+    }
+// 인터페이스 A는 인터페이스 B를 상속하고 있으므로, A는 B의 메서드인 methodB()를 상속받게 됨.
+// 또한 A는 자신만의 메서드인 methodA()를 정의할 수 있다.
+// 이렇게 인터페이스 상속을 사용하면 코드의 재사용성을 높일 수 있다. 인터페이스 상속을 통해 다양한
+// 인터페이스를 조합하여 필요한 기능을 제공하는 새로운 인터페이스를 만들 수 있다.
+//------------------------------------------------------------------------------------------------
+// 3★default 메서드와 static 메서드
+// 자바8부터 인터페이스에 default메서드와 static메서드를 선언할 수 있게 되었다. 
+// default 메서드는 인터페이스에서 구현된 메서드로, 구현 클래스에서 이를 오버라이드 할 수 있다.
+// 인터페이스를 수정하지 않고 새로운 기능을 추가할 수 있어서 유용한것.
+
+    public interface MyInterface {
+        // 추상 메서드 선언하고
+        void abstractMethod();
+
+        // defaultMethod()가 구현된 메서드로 선언되어 있다.
+        // 구현 클래스에서는 abstractMethod()를 반드시 구현해야 하지만, 
+        // defaultMethod()는 구현하지 않아도 된다.
+        default void defaultMethod() {
+            System.out.println("this is a default method.");
+        }
+    }
+
+    // 또한 static 메서드는 인터페이스에서 구현된 메서드로, 인터페이스 이름으로 직접 호출할 수 있다.
+    // 이는 인터페이스와 관련된 유틸리티 메서드를 제공할 때 유용하다.
+    // 예를들어, 다음과 같은 인터페이스가 있다고 해보자
+
+    public interface YourInterface {
+        void abstractMethod();
+
+        default void defaultMethod() {
+            System.out.println("This is a default method.");
+        }
+
+        static void staticMethod() {
+            System.out.println("This is a static method.");
+            YourInterface.staticMethod();
+        }
+        // 위 인터페이스에서는 abstractMethod()와 defaultMethod()가 이전과 동일하게 선언됨.
+        // 추가로 staticMethod()가 static 메서드로 선언 되어있음. 
+        // 인터페이스에서는 인터페이스 이름으로 'staticMethod()'를 직접 호출할 수 있다.
+        
+    }
+   
 
 }
--- basic_course/Basic08.java
+++ basic_course/Basic08.java
@@ -1,5 +1,109 @@
+// 8. 예외처리
+// 1★예외 처리 개념과 구현
+// 2★try-catch-finally 문
+// 3★예외 클래스와 예외 처리의 계층 구조
+// 4★throws 키워드
 package basic_course;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Basic08 {
-    
+
+public static void main(String[] args) {
+// 1★예외 처리 개념과 구현
+// 예외 처리(Exception Handling)는 프로그램 실행 도중 예기치 않은 상황(오류, 예외)등이 발생할 때,
+// 그 상황을 처리하는 방법을 말한다.
+// 예를 들어, 파일을 읽어들이는 코드에서 파일이 없거나 경로에 문제가 있을 경우 IOException이 발생한다.
+// 이때 IOException을 처리하지 않으면, 프로그램이 예외 상황으로 중단된다.
+
+// 2★try-catch-finally문
+// Java에서는 try-catch-finally 구문을 사용하여 예외를 처리한다. try 블록안에는 예외가 발생할 
+// 가능성이 있는 코드를 넣고, catch 블록 안에는 예외가 발생했을 때 실행될 코드를 넣습니다.
+// 마지막으로 finally 블록 안에는 예외 발생 여부와 상관없이 실행될 코드를 넣는다.
+
+// 예를 들어, 파일을 읽어들이는 코드에서 IOException이 발생할 경우, 다음과 같이 예외처리가 가능하다.
+
+
+//       try {
+//         // 파일을 읽어들이는 코드
+//      FileInputStream fis = new FileInputStream("file.txt");
+//      // ...
+//      } catch (IOException e) {
+//          // 예외가 발생하면 실행되는 코드
+//          System.out.println("파일을 읽어들이는 동안 오류가 발생했습니다.");
+
+//      } finally {
+//          // 예외 발생 여부와 상관없이 실행되는 코드
+//          // System.out.println("예외 처리가 끝났습니다.");
+//          }
 }
+// 위 코드에서 try 블록 안에서 파일을 읽어들이는 코드가 발생한다. 만약 IOException이 발생하면,
+// catch 블록 안에서 오류 메시지를 출력하고, 예외의 상세한 정보를 출력한다. 마지막으로
+// final 블록 안에는 예외 발생 여부와 상관없이 "예외 처리가 끝났습니다." 메시지를 출력하게 된다.
+
+//-----------------------------------------------------------------------------------------------
+
+// 3★예외 클래스와 예외 처리의 계층 구조
+// Java에서는 예외 클래스와 예외 처리를 위한 계층 구조를 가지고 있다. 이는 예외 처리의 유연성과 확장성을
+// 높이는데 도움이 된다. 예외 클래스는 Throwable 클래스를 상속하며, 대표적으로는 Exception과 Error이 있다.
+
+// Exception은 일반적으로 프로그램 실행 중에 발생할 수 있는 예외 상황을 나타내며, 이러한 예외는 프로그램에서
+// 처리될 수 있다. Exception 클래스는 다시 RuntimeException과 그 외의 예외로 나눈다.
+
+// RuntimeException은 주로 프로그래머의 실수에 의해 발생하는 예외이며, 컴파일러가 체크하지 않는다.
+// 대표적으로 NullPointerException, IndexOutOfBoundsException 등이 있다.
+// 그 외의 예외는 컴파일러가 체크하며, 처리가 필수적 이다. 대표적으로 IOException, ClassNotFoundException
+// 등이 있다.
+
+// Error는 일반적으로 시스템 레벨에서 발생하는 예외 상황을 나타내며, 프로그램에서 직접 처리할 수 없다. 
+// 대표적으로 OutOfMemoryError, StackOverflowError 등이 있다. 
+
+// 예외 처리의 계층 구조는 try-catch문을 사용하여 예외를 처리할 때, catch문에서 처리할 예외 클래스를 
+// 명시함으로써 구현된다. 이때, 예외 처리의 순서는 자식 클래스부터 처리하는 것이 일반적이다. 예를 들어,
+// Exception을 상속하는 IOException이 있을 경우, IOException을 먼저 처리하고, 그 다음에 Exception
+// 을 처리하는 것이 좋다.
+
+// 예외 처리의 순서는 다음과 같다.
+// 구체적인 예외를 처리하는 catch문을 먼저 작성한다.
+// 구체적인 예외를 처리하는 catch문이 없으면 상위 예외 클래스를 처리하는 catch문으로 이동한다.
+// try 블록에서 예외가 발생하지 않으면, catch문을 건너 뛰고 finally 블록으로 이동한다.
+
+
+//-----------------------------------------------------------------------------------------------
+// 4★throws 키워드
+// throw 키워드는 메서드에서 예외를 처리하지 않고 호출한 쪽으로 예외를 던지는 역할을 한다.
+// 예외를 던지는 메서드는 'throw' 키워드를 이용해 해당 예외를 던진다는 것을 알려주어야 한다.
+// 예를 들어, 다음과 같이 파일을 읽는 메서드를 만들었다고 가정해보자.
+public static void readFile(String fileName) throws FileNotFoundException, IOException {
+    FileInputStream fis = new FileInputStream(fileName);
+    // 파일을 읽는코드
+    // ..
+    fis.close();
+
+// 위 메서드에서는 'fileNotFoundException'과 'IOException'을 던질 수 있다. 
+// 하지만 이 메서드에서는 예외를 직접 처리하지 않고, 'throws' 키워드를 이용해 예외를 호출한 쪽으로 던진다.
+
+// 이제 이 메서드를 호출할 때는 다음과 같이 처리한다.
+    try {
+        readFile("file.txt");
+    } catch(FileNotFoundException e) {
+        System.out.println("파일을 찾을 수 없습니다.");
+    } catch (IOException e) {
+        System.out.println("파일을 읽는 동안 오류가 발생했습니다.");
+    }
+// 위 코드에서 readFile 메서드는 throws 키워드를 이용해 예외를 던졌기 때문에, 해당 메서드를 호출하는
+// 쪽에서는 반드시 예외를 처리해야 한다. 이를 위해 try-catch 구문을 이용해 예외 처리를 한다.
+
+// 'throws' 키워드를 이용해 예외를 던질 때, 해당 예외가 반드시 처리되어야 하는것은 아니다. 
+// 즉 'throws'키워드를 이용해 예외를 던져도 호출한 쪽에서 예외 처리를 하지 않을 수도 있다. 
+// 이 경우, 예외는 호출한 쪽으로 계속 전파되어 최종적으로는 JVM에 의해 처리된다.
+}
+
+
+    
+
+}
+
+
