dbc7421750965477a42db2c95bc8888051a30d76
Junjehyun <h94051987@gmail.com>
2023-03-29 22:48:16 +0900

    230329

---
+++ basic_course/Basic10_2.java
@@ -0,0 +1,26 @@
+package basic_course;
+
+public class Basic10_2 {
+
+    // 배열은 반복문과 함께 사용되면 매우 유용하다. 다음은 배열의 모든 값을 출력하는 예시다.
+
+    // for (int i =0; i < arr.length; i++) {
+    //     System.out.println(arr[1]);
+    // }
+
+    // 위 코드에서 'arr.length'는 배열의 길이를 의미한다.
+    // 'i'가 'arr.length'보다 작은 동안 반복하면서 배열의 값을 출력한다.
+
+    // 또한 자바에서는 배열을 초기화 하는 다양한 방법이 있다.
+
+    // 배열 생성과 동시에 초기화
+    // int[] arr = { 10, 20, 30, 40, 50 };
+
+    // 반복문을 이용한 초기화
+    // for (int i = 0; i < arr.length; i++ ) {
+    //     arr[i] = i + 1;
+    // }
+
+    // // Array 클래스의 fill 메서드를 이용한 초기화
+    // Arrays.fill(arr, 0);
+}
